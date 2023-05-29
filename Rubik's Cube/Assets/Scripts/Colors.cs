31d6e5911193a7e98f8868741e655d53fdd28638
Leonardo Valença <lpvalenca@hotmail.com>
2023-05-29 18:14:47 -0300

    Corrected comments on Colors.cs

--- Rubik's Cube/Assets/Scripts/Colors.cs
+++ Rubik's Cube/Assets/Scripts/Colors.cs
@@ -6,9 +6,9 @@ public class Colors : MonoBehaviour
     public static readonly Color Up = new(1, 1, 0.7294118f, 1); // Yellow - #FFFFBA
     public static readonly Color Left = new(0.7294118f, 1, 0.7882353f, 1); // Green - #BAFFC9
     public static readonly Color Down = Color.white; // White - #FFFFFF
-    public static readonly Color Front = new(0.972549f, 0.8588235f, 0.7294118f, 1);//new(1, 0.5f, 0, 1); // Orange - #F8DBBA
-    public static readonly Color Right = new(0.7294118f, 0.8823529f, 1, 1);//Color.blue; // Blue - #BAE1FF
-    public static readonly Color Back = new(1, 0.7019608f, 0.7294118f, 1);//Color.red; // Red - #FFB3BA
+    public static readonly Color Front = new(0.972549f, 0.8588235f, 0.7294118f, 1); // Orange - #F8DBBA
+    public static readonly Color Right = new(0.7294118f, 0.8823529f, 1, 1); // Blue - #BAE1FF
+    public static readonly Color Back = new(1, 0.7019608f, 0.7294118f, 1); // Red - #FFB3BA
 
     // Start is called before the first frame update
     void Start()
